2090919ad86ea80aa66bebbaa4d1b4d88b496be4
Pedro Luque <pedroluque481@gmail.com>
2025-09-04 13:32:40 -0300

    Correcciones y mejoras en Ejercicio 2

--- Luque Castaño, Pedro/Ejercicio 2/ejercicio2.js
+++ Luque Castaño, Pedro/Ejercicio 2/ejercicio2.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
 
@@ -8,13 +8,24 @@ app.use(express.json());
 // Array interno para almacenar los alumnos
 const alumnos = [];
 
+function estadoAlumno(promedio){
+    if (promedio < 6) {
+            return 'Reprobado';
+        } else if (promedio < 8) {
+            return 'Aprobado';
+        }   else {
+            return 'Promocionado';
+        }
+}
+
 // POST - Crear un nuevo alumno
 app.post('/alumnos', (req, res) => {
     const { nombre, notas } = req.body;
-
-    // Verificar que el alumno no exista
-    if (alumnos.find(alumno => alumno.nombre === nombre)) {
-        return res.status(400).json({ error: 'El alumno ya existe, ingrese otro' });
+    
+    // Validacion del JSON de la request
+    const keys = Object.keys(req.body)
+    if (!(keys.length === 2 && keys.includes("nombre") && keys.includes("notas"))){
+        return res.status(400).json({ error: "El JSON debe únicamente contener 'nombre' y 'notas'"});
     }
 
     // Validar que se proporcionen tres notas
@@ -22,25 +33,30 @@ app.post('/alumnos', (req, res) => {
         return res.status(400).json({ error: 'Debe proporcionar un array con exactamente tres notas' });
     }
 
+    // Validacion de que el array notas contenga SOLO valores numericos
+    const regex = /^\d+(\.\d+)?$/;
+    if (!notas.every(nota => regex.test(nota))) {
+        return res.status(400).json({ error: "Las notas deben ser valores numéricos"});
+    }
+
+    // Verificar que el alumno no exista
+    if (alumnos.find(alumno => alumno.nombre === nombre)) {
+        return res.status(400).json({ error: 'El alumno ya existe, ingrese otro' });
+    }
+
+    const notasNum = notas.map(nota => Number(nota));
+
     // Agregamose el alumno al array
-    alumnos.push({ nombre, notas });
+    alumnos.push({ nombre, notas: notasNum });
     res.status(201).json({ message: 'Alumno creado exitosamente' });
 });
 
 // GET - Obtener listado de todos los alumnos
 app.get('/alumnos', (req, res) => {
     const resultado = alumnos.map(alumno => {
-        const promedio = (alumno.notas[0] + alumno.notas[1] + alumno.notas[2]) / 3;
+        const promedio = ((alumno.notas[0] + alumno.notas[1] + alumno.notas[2]) / 3).toFixed(2);
 
-        let estado;
-
-        if (promedio < 6) {
-            estado = 'Reprobado';
-        } else if (promedio < 8) {
-            estado = 'Aprobado';
-        }   else {
-            estado = 'Promocionado';
-        }
+        let estado = estadoAlumno(promedio);
 
         return { ...alumno, promedio, estado };
     });
@@ -55,17 +71,9 @@ app.get('/alumnos/:nombre', (req, res) => {
     if (!alumnoEncontrado) {
         return res.status(404).json({error: "Alumno no encontrado"})
     } else {
-        const promedio = (alumnoEncontrado.notas[0] + alumnoEncontrado.notas[1] + alumnoEncontrado.notas[2]) / 3;
+        const promedio = ((alumnoEncontrado.notas[0] + alumnoEncontrado.notas[1] + alumnoEncontrado.notas[2]) / 3).toFixed(2);
 
-        let estado;
-
-        if (promedio < 6) {
-            estado = 'Reprobado';
-        } else if (promedio < 8) {
-            estado = 'Aprobado';
-        }   else {
-            estado = 'Promocionado';
-        }
+        let estado = estadoAlumno(promedio);
 
         const resultado = {...alumnoEncontrado, promedio, estado}
         
